e1e75d82ab4c6f607298881e8cfccedafe119236
Goffin Simon <simon.goffin@outlook.com>
2014-07-24 15:18:56 +0200

    Qweb version correcte.

--- addons/website_version/models/version.py
+++ addons/website_version/models/version.py
@@ -2,19 +2,19 @@
 from openerp.osv import osv,fields
 
 
-class view_version(osv.osv):
+class ViewVersion(osv.Model):
     _inherit = "ir.ui.view"
     
     _columns = {
         'master_id':fields.many2one('ir.ui.view',string="Source View"),
-        'version_ids': fields.one2many('ir.ui.view', 'master_id',string="Old Views"),
+        'version_ids': fields.one2many('ir.ui.view', 'master_id',string="Old Views",copy=True),
     }
     
     def write(self, cr, uid, ids, vals, context=None):
-        from pudb import set_trace; set_trace()
+        #from pudb import set_trace; set_trace()
         for view in self.browse(cr, uid, ids, context=context):
             if view.type == 'qweb' and vals.get('arch'):
                 old_id=self.copy(cr,uid,view.id,{})
-                super(view_version, self).write(cr, uid,[old_id], dict(version_ids=view), context=context)
+                super(ViewVersion, self).write(cr, uid,[old_id], {'version_ids': [(4, old_id)]}, context=context)
                 vals['master_id'] = old_id
-        super(view_version, self).write(cr, uid, ids, vals, context=context)
+        super(ViewVersion, self).write(cr, uid, ids, vals, context=context)
