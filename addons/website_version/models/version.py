4d096ee45dc6b1dfdfe6bd596ed8203f23c209c4
Goffin Simon <simon.goffin@outlook.com>
2014-07-24 13:13:51 +0200

    Website button bug.

--- addons/website_version/models/version.py
+++ addons/website_version/models/version.py
@@ -11,22 +11,10 @@ class view_version(osv.osv):
     }
     
     def write(self, cr, uid, ids, vals, context=None):
+        from pudb import set_trace; set_trace()
         for view in self.browse(cr, uid, ids, context=context):
             if view.type == 'qweb' and vals.get('arch'):
-                old_view_vals = view.read([
-                    'name',
-                    'model',
-                    'priority',
-                    'type',
-                    'arch',
-                    'field_parent',
-                    'xml_id',
-                    'create_date',
-                    'write_date',
-                    'mode',
-                    'application'
-                ])[0]
-                old_view_vals['version_ids'] = view
-                old_id = self.create(cr, uid, old_view_vals, context=context)
+                old_id=self.copy(cr,uid,view.id,{})
+                super(view_version, self).write(cr, uid,[old_id], dict(version_ids=view), context=context)
                 vals['master_id'] = old_id
         super(view_version, self).write(cr, uid, ids, vals, context=context)
