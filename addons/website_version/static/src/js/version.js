8dbecdbae6432ee0995c7ee58ecce8aa6b967c7f
Goffin Simon <simon.goffin@outlook.com>
2014-07-26 18:52:17 +0200

    Brouillon_4.

--- addons/website_version/static/src/js/version.js
+++ addons/website_version/static/src/js/version.js
@@ -1,13 +1,29 @@
-(function () {
-    'use strict';
-    var website = openerp.website;
-    website.add_template_file('/website_version/static/src/xml/previous_button.xml');
-    website.EditorBar.include({
-        start: function () {
-            var self = this;
-            console.log("Are you ready!");
-            return this._super();
+openerp.website_version = function(instance) {
+    var _t = instance.web._t,
+        _lt = instance.web._lt;
+    var QWeb = instance.web.qweb;
+
+    instance.website_version = {};
+
+    instance.website_version.action = instance.web.Widget.extend({
+        template: "website.user_navbar",
+        init: function(parent) {
+            this._super(parent);
         },
+        start: function() {
+            $("button").click(function() {
+                console.log("someone clicked on the button");
+                
+                
+                
+            });
+            
+                
+            },
+    });
+    
+    $( document ).ready(function() {
+    console.log( "ready!" );
     });
         
 })();
