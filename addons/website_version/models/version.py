59c06728f3f69d238c2cd53b3b3e95ed63f60982
Goffin Simon <simon.goffin@outlook.com>
2014-08-05 09:55:02 +0200

    Version simple propre.

--- addons/website_version/models/version.py
+++ addons/website_version/models/version.py
@@ -22,25 +22,26 @@ class ViewVersion(osv.Model):
         try:
             snapshot_id=request.session['snapshot_id']
             if snapshot_id==0:
-                raise 
-            snap = request.registry['website_version.snapshot']
-            snapshot=snap.browse(cr, uid, [snapshot_id], context=context)[0]
-            snap_ids=[]
-            no_snap_ids=[]
-            for id in ids:
-                check=True
-                for view in snapshot.view_ids:
-                    if view.master_id.id == id:
-                        snap_ids.append(view.id)
-                        check=False
-                if check:
-                    copy_id=self.copy(cr,uid,id,{})
-                    super(ViewVersion, self).write(cr, uid, copy_id, {'master_id':id,'snapshot_id':snapshot_id}, context=context)
-                    super(ViewVersion, self).write(cr, uid,[id], {'version_ids': [(4, copy_id)]}, context=context)
-                    snap.write(cr, uid,[snapshot_id], {'view_ids': [(4, copy_id)]}, context=context)
-                    snap_ids.append(copy_id)
+                super(ViewVersion, self).write(cr, uid, ids, vals, context=context)
+            else:
+                snap = request.registry['website_version.snapshot']
+                snapshot=snap.browse(cr, uid, [snapshot_id], context=context)[0]
+                snap_ids=[]
+                no_snap_ids=[]
+                for id in ids:
+                    check=True
+                    for view in snapshot.view_ids:
+                        if view.master_id.id == id:
+                            snap_ids.append(view.id)
+                            check=False
+                    if check:
+                        copy_id=self.copy(cr,uid,id,{})
+                        super(ViewVersion, self).write(cr, uid, copy_id, {'master_id':id,'snapshot_id':snapshot_id}, context=context)
+                        super(ViewVersion, self).write(cr, uid,[id], {'version_ids': [(4, copy_id)]}, context=context)
+                        snap.write(cr, uid,[snapshot_id], {'view_ids': [(4, copy_id)]}, context=context)
+                        snap_ids.append(copy_id)
 
-            super(ViewVersion, self).write(cr, uid, snap_ids, vals, context=context)
+                super(ViewVersion, self).write(cr, uid, snap_ids, vals, context=context)
             
         except:
             super(ViewVersion, self).write(cr, uid, ids, vals, context=context)
@@ -48,14 +49,12 @@ class ViewVersion(osv.Model):
     def read(self, cr, uid, ids, fields=None, context=None, load='_classic_read'):
         #from pudb import set_trace; set_trace()
         try :
-            iuv = request.registry['ir.ui.view']
-            iuv.clear_cache()
+            self.clear_cache()
             snapshot_id=request.session['snapshot_id']
             if snapshot_id==0:
-                raise 
+                return super(ViewVersion, self).read(cr, uid, ids, fields=fields, context=context, load=load)
             snap = request.registry['website_version.snapshot']
             snapshot=snap.browse(cr, uid, [snapshot_id], context=context)[0]
-            #print 'SNAPSHOT NAME={}'.format(snapshot.name)
             snap_ids=[]
             snap_trad={}
             for id in ids:
