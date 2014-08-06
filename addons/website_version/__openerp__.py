11e4da8e403baee500ab04755e7193f10012a32a
Goffin Simon <simon.goffin@outlook.com>
2014-08-06 11:53:14 +0200

    Backend menu website/snapshot.

--- addons/website_version/__openerp__.py
+++ addons/website_version/__openerp__.py
@@ -9,12 +9,13 @@ OpenERP Website CMS
 
         """,
     'author': 'OpenERP SA',
-    'depends': ['website'],
+    'depends': ['website','marketing'],
     'installable': True,
     'data': [
         'security/ir.model.access.csv',
         'views/website_version_assets.xml',
         'views/menu.xml',
+        'views/marketing_view.xml',
     ],
     'demo': [],
     'qweb': [],
