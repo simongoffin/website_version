858bdbf161ffd8df706fccd7c6bc1cef19b9c4fc
Goffin Simon <simon.goffin@outlook.com>
2014-08-11 17:15:18 +0200

    Four.

--- addons/website_version/static/src/js/menu_version.js
+++ addons/website_version/static/src/js/menu_version.js
@@ -10,39 +10,33 @@
     website.EditorBarContent.include({
         start: function() {
             $('#version-menu-button').click(function() {
-                console.log("Version clicked!");
                 openerp.jsonRpc( '/all_snapshots', 'call', 
                 {})
                 
                 .then(function (result) {
-                    console.log(result);
                     if($(".all_versions").length > 0){
                         $(".all_versions").html().replace(QWeb.render("all_versions", {mytab:result}));
                     }
                     else{
                         $( ".snapshot" ).append(QWeb.render("all_versions", {mytab:result}));
                     }
-                    console.log(result);
                  })                
             });
             return this._super();
         },
         
         snapshot: function() {
-            console.log("Snapshot!");
             website.prompt({
                 id: "editor_new_snapshot",
                 window_title: _t("New snapshot"),
                 input: "Snapshot name",
             }).then(function (name) {               
-                console.log(name);
                 var context = website.get_context();
                 openerp.jsonRpc( '/create_snapshot', 'call', 
                 {
                     'name':name,
                 })
                 .then(function (result) {
-                    console.log('Snapshot '+name+' saved');
                     location.reload();
                 })                               
             });
@@ -50,31 +44,26 @@
         
         change_snapshot: function() {
             var text = $(event.target).text();
-            console.log('Youp clicked on ' + text);
             openerp.jsonRpc( '/change_snapshot', 'call', 
                 {
                     'snapshot_name':text,
                 })
                 .then(function (result) {
-                    console.log(result);
                     location.reload();
                 })
         },
 
         delete_snapshot: function() {
             var text = $(event.target).text();
-            console.log('Youp clicked on ' + text);
             openerp.jsonRpc( '/delete_snapshot', 'call', 
                 {})
                 .then(function (result) {
-                    console.log(result);
                     location.reload();
                 })
         },
     });
     
     website.ready().done(function() {
-        console.log("This is the menu_version!");
     });
     
 })();
